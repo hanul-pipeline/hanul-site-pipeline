466120bdc64c0ec545cc6a6623fa17c41ba292e4
blahxblah <id.blahxblah@gmail.com>
2023-07-12 20:21:25 +0900

    curl_gen

---
+++ src/flask/curl_gen.py
@@ -0,0 +1,17 @@
+import subprocess
+import time
+from datetime import datetime
+
+def send_curl_every_second():
+    while True:
+        subprocess.run([
+            'curl', '-X', 'POST', 
+            '-H', 'Content-Type: application/json', 
+            '-d',f'{{"sensor_id": 1, "date":"{datetime.now()}", "gas_level": 50}}', 
+            'http://192.168.70.89:9000/data-endpoint'
+        ])
+        time.sleep(1)  # 1초 동안 대기
+
+# 함수 실행
+send_curl_every_second()
+
