7196b4daf408c0edf8fd28acfe40196d5eafc54c
blahxblah <blahxblah@gmail.com>
2023-07-15 19:01:55 +0900

    json_to_table_ver2 추가. 특정 시간대 json파일만 보낼 수 있도록

--- lib/MySQL/connector_lib.py
+++ lib/MySQL/connector_lib.py
@@ -1,4 +1,6 @@
 import mysql.connector, json, csv
+import glob
+from datetime import datetime, timedelta
 
 # CSV File Located At Local
 PATH_DB = '/Users/kimdohoon/src/google/hanul_sql.csv'
@@ -30,4 +32,29 @@ def JSON_to_table(JSON_DIR, TABLE_NAME):
     query = f"INSERT INTO {TABLE_NAME} (sensor_id, date, time, value) \
               VALUES ({sensor_id}, {date}, {time}, {value})"
     cursor.execute(query)
+    conn.commit()
+    
+# UPDATE DATABASE VER.2
+def JSON_to_table_ver2(JSON_DIR, TABLE_NAME):
+    conn = mysql_connect()
+    cursor = conn.cursor()
+    # 현재 시간과 한 시간 전 시간을 계산
+    now = datetime.now().replace(minute=0, second=0, microsecond=0) # 현재 시간을 추출해서 정각으로 변환
+    one_hour_ago = now - timedelta(hours=1)
+
+    # 디렉토리 안의 모든 json 파일을 읽음
+    for filename in glob.glob(JSON_DIR):
+        # 파일명에서 시간 정보를 추출
+        timestamp_str = filename.split('/')[-1].split('&')[1] + '&' + filename.split('/')[-1].split('&')[2].split('.')[0]  # 'date&time' 형식
+        timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d&%H:%M:%S')  # 날짜와 시간 형식에 따라서 수정해야 함
+
+        # 파일의 시간이 한 시간 전과 현재 사이에 있는 경우에만 처리
+        if one_hour_ago <= timestamp < now:
+            with open(filename, 'r') as f:
+                # json 파일을 파싱
+                data = json.load(f)
+
+                # MySQL 쿼리 생성
+                sql = f"INSERT INTO {TABLE_NAME}(sensor_id, date, time, value) VALUES (%s, %s, %s, %s)"
+                cursor.execute(sql, (data['sensor_id'], data['date'], data['time'], data['value']))
     conn.commit()
