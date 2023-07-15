e9557dacf1dda035b731bcb616762fca6b2649fc
blahxblah <blahxblah@gmail.com>
2023-07-15 17:08:10 +0900

    주석수정

--- src/flask/curl_gen.py
+++ src/flask/curl_gen.py
@@ -5,12 +5,12 @@ def send_curl_every_second():
     gas_level = 100 * random.random()
     while True:
         # SEND DATA TYPE : curl -X POST -H "Content-Type: application/json" -d \
-        # '{"sensor_id": 1, "date":"2023-07-03:20:04:30", "gas_level": 50}' \
+        # '{"sensor_id": 1, "date":"2023-07-03", "time":"20:04:30", "gas_level": 50}' \
         # http://192.168.70.89:9000/data-endpoint
         subprocess.run([
             'curl', '-X', 'POST', 
             '-H', 'Content-Type: application/json', 
-            '-d',f'{{"sensor_id": 1, "date":"{datetime.now().strftime("%Y-%m-%d")}", "time":"{datetime.now().strftime("%H:%M:%S")}", "gas_level": 50}}', 
+            '-d',f'{{"sensor_id": 1, "date":"{datetime.now().strftime("%Y-%m-%d")}", "time":"{datetime.now().strftime("%H:%M:%S")}", "gas_level": {gas_level}}}', 
             'http://192.168.70.89:9000/data-endpoint'
         ])
         time.sleep(1)  # 1초 동안 대기
