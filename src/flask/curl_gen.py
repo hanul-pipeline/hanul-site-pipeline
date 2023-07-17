da2812a4b7515ce5afdc21110d061ec86fbb9cb1
hooniegit <130134750+hooniegit@users.noreply.github.com>
2023-07-17 19:56:12 +0900

    curl job fixed

--- src/flask/curl_gen.py
+++ src/flask/curl_gen.py
@@ -9,10 +9,10 @@ def send_curl_every_second(sensor_id, sensor_value, interval):
         subprocess.run([
             'curl', '-X', 'POST', 
             '-H', 'Content-Type: application/json', 
-            '-d',f'{{"{sensor_id}": 1, "date":"{datetime.now().strftime("%Y-%m-%d")}", "time":"{datetime.now().strftime("%H:%M:%S")}", "sensor_value": {sensor_value}}}', 
+            '-d',f'{{"sensor_id": {sensor_id}, "date":"{datetime.now().strftime("%Y-%m-%d")}", "time":"{datetime.now().strftime("%H:%M:%S")}", "sensor_value": {sensor_value}}}', 
             'http://192.168.70.89:9000/data-endpoint'
         ])
-        time.sleep(interval)  # n초 동안 대기
+        time.sleep(interval)
 
 # 함수 실행
-send_curl_every_second('sensor_id', 'sensor_value', 'interval')
+send_curl_every_second(sensor_id='1', sensor_value=100, interval=3)
