f000c05175c07e84cef5e1ed0a5f8ca9301da096
hooniegit <130134750+hooniegit@users.noreply.github.com>
2023-07-31 20:18:09 +0900

    230731/hooniegit - 데이터 적재 메소드 수정 : JSON -> SQLite

--- src/flask/curl.py
+++ src/flask/curl.py
@@ -3,14 +3,14 @@ from flask import Flask, request
 
 # LIBRARY IMPORT
 import os, sys
-os.chdir('/Users/kimdohoon/git/hanul/hanul-site-pipeline')
+os.chdir('/flask_compose')
 lib_dir = f"{os.getcwd()}/lib"
 sys.path.append(lib_dir)
 from MySQL.connector_lib import *
 from processing.rank import *
 
 #
-SQLite_DIR = '/Users/kimdohoon/git/hanul/hanul-site-pipeline/datas/SQLite/cite'
+SQLite_DIR = '/flask_compose/datas/SQLite/cite'
 
 # CREATE APP
 app = Flask(__name__)
@@ -35,7 +35,7 @@ def receive_data():
 
     # INSERT DATAS INTO SQLITE + CREATE FLAG
     SQLite_UPDATE(SQLite_DIR, QUERY)
-    LOG_DIR = f"/Users/kimdohoon/git/hanul/hanul-site-pipeline/datas/DONE/{sensor_id}"
+    LOG_DIR = f"/flask_compose/datas/DONE/{sensor_id}"
 
     # CREATE FLAG
     with open(f"{LOG_DIR}/{sensor_id}&{date}&{time}&DONE", "w") as file:
