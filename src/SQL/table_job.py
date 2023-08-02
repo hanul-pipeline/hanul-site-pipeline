268b372f20ee3f16d020867911926383c2ad4612
blahxblah <id.blahxblah@gmail.com>
2023-08-02 20:28:36 +0900

    table_job.py 수정 중

--- src/SQL/table_job.py
+++ src/SQL/table_job.py
@@ -9,7 +9,7 @@ import connector_lib as lib
 # 비교 끝나면 JSON 들어가서 해당 시간인 데이터 추리기
 # 그 데이터들만 읽어서 서버에 올리기
 
-# PARAMETERS
+# SQLite_to_MySQL 파라미터
 sensor_id = sys.argv[1]
 # JSON_DIR = f"../../datas/JSON/{sensor_id}"
 TABLE_NAME = 'measurement'
@@ -21,7 +21,12 @@ mysql_config = {
     'port':'3307',
     'db': 'kjh'
 }
+# check_flag 파라미터
+LOG_DIR = f"/home/kjh/code/hanul-site-pipeline/datas/DONE/{sensor_id}"
+check_time = 3600
+interval = 1
 
 # UPDATE DATABASE
 #lib.JSON_to_table(JSON_DIR, TABLE_NAME)
+check_done = check_flag(LOG_DIR, check_time, interval)
 lib.SQLite_to_MySQL(SQLite_DIR, mysql_config, TABLE_NAME)
