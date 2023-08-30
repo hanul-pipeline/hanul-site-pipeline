4f73233f77bc788c7ce353a7cf86c35c0ce77722
hooniegit <130134750+hooniegit@users.noreply.github.com>
2023-08-30 19:00:04 +0900

    230830/hooniegit - sqlite 파일 생성 로직 작성

--- src/DB/create_db.py
+++ src/DB/create_db.py
@@ -1,28 +1,23 @@
 from datetime import datetime, timedelta
-# from libs.DB.SQLite import *
 import configparser, sqlite3
 
 config = configparser.ConfigParser()
-# config.read("/hanul/config/config.ini")
-config.read("/Users/kimdohoon/git/hanul/hanul-site-pipeline/config/config.ini")
+config.read("/hanul/config/config.ini")
+config.read("/Users/kimdohoon/git/hanul/hanul-site-pipeline/config/config.ini") # test
 sensor_list = config.get("Sensor", "sensor_list")
 sensors = [str(sensor) for sensor in sensor_list.split(',')]
 
-# yymmdd_HH_404
 date = (datetime.now() + timedelta(hours=1)).strftime("%y%m%d_%H")
 print(date)
 
 for sensor in sensors:
-    # 테스트 경로(로컬)
-    path = f"/Users/kimdohoon/git/hanul/hanul-site-pipeline/datas/SQLite/{sensor}/{date}_{sensor}"
-    # 실제 경로
-    # path = f"/hanul/datas/SQLite/{sensor}/{date}_{sensor}"
+    path = f"/hanul/datas/SQLite/{sensor}/{date}_{sensor}"
+    path = f"/Users/kimdohoon/git/hanul/hanul-site-pipeline/datas/SQLite/{sensor}/{date}_{sensor}" # test
+
     with open(path, "w") as file:
-            # 데이터베이스 연결 생성 또는 기존 데이터베이스 연결
         conn = sqlite3.connect(path)
         cursor = conn.cursor()
 
-        # 테이블 생성 쿼리
         QUERY = '''
         CREATE TABLE IF NOT EXISTS measurement (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
